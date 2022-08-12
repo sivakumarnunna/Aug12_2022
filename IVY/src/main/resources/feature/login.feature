335d8f02d022660a0eb3a1e78c3bbc40cbc301b1
sivak <sivak@Sivakumar-Nunna.home>
2022-08-12 13:31:58 +0530

    Added feature file and step definition file

---
+++ IVY/src/main/resources/feature/login.feature
@@ -0,0 +1,7 @@
+Feature: Bwin Navigations
+
+Scenario: Login to bwin.com
+Given open bwin hpe page
+When click on login button
+Then login page shold open successfully
+When enter username "USERNAME" password "PASSWORD"
---
+++ IVY/src/test/java/com/bwin/IVY/stepdefinitions/BwinLogin.java
@@ -0,0 +1,35 @@
+package com.bwin.IVY.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class BwinLogin {
+	
+	
+	@Given("open bwin hpe page")
+	public void open_bwin_hpe_page() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("click on login button")
+	public void click_on_login_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("login page shold open successfully")
+	public void login_page_shold_open_successfully() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("enter username {string} password {string}")
+	public void enter_username_password(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+
+}
