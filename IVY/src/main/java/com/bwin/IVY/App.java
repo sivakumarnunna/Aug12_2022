a969a41c3004f0d8ae4dcaf325376aa7ed4ee160
sivak <sivak@Sivakumar-Nunna.home>
2022-08-12 12:49:50 +0530

    first commit

---
+++ IVY/src/main/java/com/bwin/IVY/App.java
@@ -0,0 +1,13 @@
+package com.bwin.IVY;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
